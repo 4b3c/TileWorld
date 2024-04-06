256f0e2fcfca48785b12c88c988abe6328728a82
Abram <58718191+4b3c@users.noreply.github.com>
2024-04-06 14:30:10 -0700

    commented code and removed edge scrolling
    now the player doesnt move, the map moves backwards, the problem came from velocity of 1 being split into 0.707 for the player and the map, giving an illusion of it slowing down

--- player.py
+++ player.py
@@ -4,25 +4,28 @@ import constants as cts
 
 class player:
 	def __init__(self, pos, radius, color):
+		# Set initial variables
 		self.pos = list(pos)
 		self.vel = [0, 0]
 		self.radius = radius
 		self.color = color
 
+		# Create a transparent surface with a circle on to represent the player
 		self.surface = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
 		pygame.draw.circle(self.surface, self.color, (self.radius, self.radius), self.radius)
 
 	def move(self, dist):
+		# Simply increment the position by the desired distance (x, y)
 		self.pos = cts.add(self.pos, dist)
 
 	def draw(self, screen, world):
+		# Cap tha magnitude of the velocity to prevent diagonal movement from being faster than cardinal movement
 		velmag = math.sqrt(self.vel[0]**2 + self.vel[1]**2)
 		if (velmag > cts.MAXSPEED):
 			self.vel = [self.vel[0] * cts.MAXSPEED / velmag, self.vel[1] * cts.MAXSPEED / velmag]
-		if (self.pos[0] + self.vel[0] > cts.BOUNDSX[0] and self.pos[0] + self.vel[0] < cts.BOUNDSX[1] \
-	  		and self.pos[1] + self.vel[1] > cts.BOUNDSY[0] and self.pos[1] + self.vel[1] < cts.BOUNDSY[1]):
-			self.move(self.vel)
-		else:
-			world.move(self.vel)
+
+		world.move(self.vel)
+
+		# Apply "friction" so the player slows to a stop smoothly
 		self.vel = [self.vel[0] * (1 - cts.FRICTION), self.vel[1] * (1 - cts.FRICTION)]
 		screen.blit(self.surface, self.pos)
--- tilemap.py
+++ tilemap.py
@@ -4,40 +4,48 @@ import constants as cts
 
 class tilemap():
 	def __init__(self, pos):
-		self.zero_pos = pos
+		# Set windowpos which is a relative position and is how rendered chunk placement is calculated
+		self.windowpos = pos
+		
+		# Creates a dictionary of rendered chunks where the key is their relative position (x, y)
+		# Rendered chunks are a 4x4 grid, where each chunk is half the width and height of the game window
 		self.chunks = {(x, y): chunk(cts.CHUNKSIZE) for x in range(-1, 3) for y in range(-1, 3)}
-
 		for mapchunk in self.chunks:
 			self.chunks[mapchunk].checkerboard()
 
 	def draw(self, screen):
+		# Draws each chunk based on its key (x, y) and multiplying that by it's size, then adding the window position
 		for mapchunk in self.chunks:
-			screen.blit(self.chunks[mapchunk], cts.add(self.zero_pos, cts.muliply(mapchunk, cts.CHUNKSIZE)))
+			screen.blit(self.chunks[mapchunk], cts.add(self.windowpos, cts.muliply(mapchunk, cts.CHUNKSIZE)))
     
 	def move(self, dist):
-		self.zero_pos = cts.subtract(self.zero_pos, dist)
-		if (self.zero_pos[0] < 0):
-			self.zero_pos[0] += cts.CHUNKSIZE[0]
+		# Updates the window position, but if we are near an edge of the rendered chunks, unloads and loads the necessary ones
+		self.windowpos = cts.subtract(self.windowpos, dist)
+		if (self.windowpos[0] < 0):
+			self.windowpos[0] += cts.CHUNKSIZE[0]
 			self.shiftchunks(-1, 0)
-		elif (self.zero_pos[0] > cts.CHUNKSIZE[0]):
-			self.zero_pos[0] -= cts.CHUNKSIZE[0]
+		elif (self.windowpos[0] > cts.CHUNKSIZE[0]):
+			self.windowpos[0] -= cts.CHUNKSIZE[0]
 			self.shiftchunks(1, 0)
 
-		if (self.zero_pos[1] < 0):
-			self.zero_pos[1] += cts.CHUNKSIZE[1]
+		if (self.windowpos[1] < 0):
+			self.windowpos[1] += cts.CHUNKSIZE[1]
 			self.shiftchunks(0, -1)
-		elif (self.zero_pos[1] > cts.CHUNKSIZE[1]):
-			self.zero_pos[1] -= cts.CHUNKSIZE[1]
+		elif (self.windowpos[1] > cts.CHUNKSIZE[1]):
+			self.windowpos[1] -= cts.CHUNKSIZE[1]
 			self.shiftchunks(0, 1)
 
 
 	def shiftchunks(self, x, y):
+		# Given the 4x4 grid of rendered chunks, we move each row and/or column to the one behind it
 		for col in cts.CHUNKSHIFT[x]:
 			for row in cts.CHUNKSHIFT[y]:
+				# This fails if we try to move a row/column behind the first one or after the last one 
 				try:
 					mapchunk = self.chunks[(row + x, col + y)]
 					print("worked:", end="")
 				except:
+					# TODO: instead of just creating a new chunk object, we need to load the correct one
 					mapchunk = chunk(cts.CHUNKSIZE)
 					mapchunk.checkerboard()
 					print("failed:", end="")
@@ -46,7 +54,6 @@ class tilemap():
 
 
 class chunk(pygame.Surface):
-
 	def checkerboard(self):
 		ts = cts.TILESIZE
 		for i in range(int(self.get_width() / ts) + 1):
